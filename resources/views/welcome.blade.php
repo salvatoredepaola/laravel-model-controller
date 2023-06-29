e8c09feeeaa65a8a3d050f14f7bde559daccc466
salvatoredepaola <salvatoredepaola8@gmail.com>
2023-06-29 22:42:26 +0200

    primo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,21 @@
             </div>
         </div>
     </div>
+    <div class="row row-cols-5 g-4">
+        @foreach ($movies as $movie)
+        <div class="col">
+            <a href="/">
+            <div class="card h-100">
+                <img src="{{$movie->img}}" class="card-img-top" alt="{{$movie->title}}">
+                <div class="card-body">
+                    <p class="card-text">Nome: {{$movie->title}}</p> 
+                    <p class="card-text">Voto: {{$movie->vote}}</p> 
+                </div>
+            </div>
+            </a>
+        </div>
+        @endforeach
+    </div>
 
 </div>
 @endsection
